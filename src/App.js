64fee6586c110cf6e740032278cc6af9e9f76c58
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-23 11:28:12 +0800

    add react-router for 3 pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Cars from "./pages/Cars";
 import DetailCar from "./pages/DetailCar";
 import Home from "./pages/Home";
+import  {Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
       <div>
-        <Home />
-        <Cars />
-        <DetailCar />
+        <Routes>
+          <Route path = "/" element={<Home />}/>
+          <Route path = "/carimobil" element={<Cars />}/>
+          <Route path = "/detailmobil" element={<DetailCar />} />
+        </Routes>
       </div>
   );
 }
