83a2a584b108a8e7a24c8e656e206061ad3a77bf
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-23 11:07:39 +0800

    add 3 pages

---
+++ src/pages/Cars/index.js
@@ -0,0 +1,9 @@
+const Cars = () => {
+    return (
+        <div>
+            <h1>Ini cars page</h1>
+        </div>
+    )
+}
+
+export default Cars;
---
+++ src/pages/DetailCar/index.js
@@ -0,0 +1,9 @@
+const DetailCar = () => {
+    return (
+        <div>
+            <h1>Ini detail car page</h1>
+        </div>
+    )
+}
+
+export default DetailCar;
