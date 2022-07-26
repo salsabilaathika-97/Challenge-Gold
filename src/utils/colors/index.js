400e0beacf4b98a9102e3e7ad8937929aa020b92
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-26 11:21:05 +0800

    add colors folder for styling

---
+++ src/utils/colors/index.js
@@ -0,0 +1,19 @@
+const mainColors = {
+    white: 'FFFFFF',
+    darkblue01 : '#CFD4ED',
+    darkblue02 : '#F1F3FF',
+    darkblue03 : '#5E70C4',
+    darkblue04 : '#0D28A6',
+    darkblue05 : '#091B6F',
+    limegreen01 : '#DEF1DF',
+    limegreen02 : '#C9E7CA',
+    limegreen03 : '#92D094',
+    limegreen04 : '#5CB85F',
+    limegreen05 : '#3D7B3F'
+}
+
+export const colors = {
+    bgMainPage01 : mainColors.darkblue02,
+    bgMainPage02 : mainColors.white,
+    bgCarMain : mainColors.darkblue04
+}
