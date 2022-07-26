7709e42dbc39dbbc4c122f73ee973dd87dd2c15a
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-26 22:30:08 +0800

    test render data from cars api

--- src/pages/Cars/index.js
+++ src/pages/Cars/index.js
@@ -83,6 +83,21 @@ const Cars = () => {
                     </Form>
                 </div>
                 </div>
+                {
+                    !!data.length && (
+                        data.map(item => (
+                            <div>
+                            <div>
+                                <img src = {item.image} />
+                            </div>
+                            <div>
+                                <h1>{item.name}</h1>
+                                <p>{item.price}</p>
+                            </div>
+                            </div>
+                        ))
+                    )
+                }
             <Footer />
         </div>
     )
