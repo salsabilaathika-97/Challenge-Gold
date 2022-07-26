7098d8b32964dfaff8dda976a791a0f0e9f5fac6
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-26 22:56:59 +0800

    change background color for detail car

--- src/pages/DetailCar/index.js
+++ src/pages/DetailCar/index.js
@@ -1,10 +1,24 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import { colors } from "../../utils";
 
 const DetailCar = () => {
+    const styles = {
+        container01 : {
+            backgroundColor: colors.bgMainPage01,
+            height: 'auto',
+            overflowX: 'hidden',
+            paddingTop: 97
+        }
+    };
+
     return (
         <div>
             <Navbar />
+            <div style={styles.container01}>
+                
+                
+            </div>
             <Footer />
         </div>
     )
