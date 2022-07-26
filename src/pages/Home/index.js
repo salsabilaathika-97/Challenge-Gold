39c23489f030a47b6fd02b51064e85abec0cba5f
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-26 11:39:29 +0800

    delete banner component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,10 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import Banner from "../../components/Banner";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
-            <Banner />
             <Footer />
         </div>
     )
