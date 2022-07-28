40b527455296424b39253d4d73a4bbe27b6a46f9
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-28 23:46:47 +0800

    remove border in nav list

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -27,7 +27,7 @@ const Navbar = () => {
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </button>
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-[#F1F3FF] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-[#F1F3FF] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 {
                     navList.map((item) => (
                         <li>
