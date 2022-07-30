86ad6612bb0e0c6e442a3f15be42fd462fe5f13b
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-30 13:39:43 +0800

    remove unused car icon

--- src/asset/index.js
+++ src/asset/index.js
@@ -7,6 +7,5 @@ import IconPrice from './icon-price.png'
 import IconProfessional from './icon-professional.png'
 import IconComplete from './icon-complete.png'
 import Testi from './testi.png'
-import IconCar from './car_icon.png'
 
-export {img_car, binar_icon, ILWoman, IconCheck, Icon24, IconPrice, IconProfessional, IconComplete, Testi, IconCar}
+export {img_car, binar_icon, ILWoman, IconCheck, Icon24, IconPrice, IconProfessional, IconComplete, Testi}
